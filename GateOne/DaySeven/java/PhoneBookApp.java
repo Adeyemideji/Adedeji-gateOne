a56c56aa835c1f3e8c28bd4fc94772e71b8474f5
Adeyemideji <adedejiadeyemi627@gmail.com>
2024-05-13 15:01:28 -0800

    part of the phone book task in java

---
+++ GateOne/DaySeven/java/PhoneBookApp.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class PhoneBookApp{
+
+public static void main(String[] args){
+
+Scannner input = new Scanner(System.in);
+
+	String prompt = """
+	phonebook menu
+	list of menu options
+
+	1 -> Add Contact
+	2 -> Remove Contact
+	3 -> Find Contact By Phone Number
+	4 -> Find Contact By First Name
+	5 -> Find Contact By Last Name
+	6 -> Edit Contact 
+	""";
+
+		String AddContactPrompt = """
+		1 -> Contact Name
+		2 -> Contact Address
+		3 -> Contact Number
+		4 -> Contact Email
+		5 -> Save
+		6 -> Back
+		""";
+
+	String RemoveContactPrompt = """
+	1 -> Search for Contact Name
+	2 -> Delete Contact
+	3 -> Back
+	""";
+
+		
+
+
+
+
+}
+
+
+}
