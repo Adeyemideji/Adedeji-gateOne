01bf12fd8a7704ff2773d415995f913ba08063ac
Adeyemideji <adedejiadeyemi627@gmail.com>
2024-05-14 13:24:09 -0800

    continuation

--- GateOne/DaySeven/java/PhoneBookApp.java
+++ GateOne/DaySeven/java/PhoneBookApp.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class PhoneBookApp{
 
@@ -6,10 +7,29 @@ public static void main(String[] args){
 
 Scanner input = new Scanner(System.in);
 
+ArrayList<String> names = new ArrayList<String>();
+
+ArrayList<Integer> phoneNumbers = new ArrayList<Integer>();
+
+String prompt = """
+phonebook menu
+list of menu options
+_____________________________________
+
+1 -> Add Contact
+2 -> Remove Contact
+3 -> Find Contact By Phone Number
+4 -> Find Contact By First Name
+5 -> Find Contact By Last Name
+6 -> Edit Contact 
+""";
+
+
 System.out.println("                     WELCOME TO MY PHONEBOOK ");
 
 System.out.print("Enter Contact First Name: ");
 String firstName = input.nextLine();
+
 
 System.out.print("Enter Contact Last Name: ");
 String lastName = input.nextLine();
@@ -17,32 +37,11 @@ String lastName = input.nextLine();
 System.out.print("Enter Contact phone Number: ");
 double contactNumber = input.nextDouble();
 
+
 System.out.println("Saving Contact >>>>>>>>>>>>>>>>>>>>>>>>");
 
 System.out.println("Saved Successfully!!!!!!!!!!!!!");
 
-	String prompt = """
-	phonebook menu
-	list of menu options
-
-	_____________________________________
-
-	1 -> Add Contact
-	2 -> Remove Contact
-	3 -> Find Contact By Phone Number
-	4 -> Find Contact By First Name
-	5 -> Find Contact By Last Name
-	6 -> Edit Contact 
-	""";
-
-	String AddContactPrompt = """
-	1 -> Contact Name
-	2 -> Contact Address
-	3 -> Contact Number
-	4 -> Contact Email
-	5 -> Save
-	6 -> Back
-	""";
 
 
 }
